743f9dfaac1053cc6e8c92b21b9c3853d859063a
Zack <zack.strubel@gmail.com>
2023-10-11 12:47:09 +0100

    Config response UI

--- console/src/routes/+page.svelte
+++ console/src/routes/+page.svelte
@@ -14,31 +14,33 @@
 	<meta name="description" content="Trigger task flows" />
 </svelte:head>
 
-<HopsNav />
+<div class="dark:bg-grain bg-cover bg-norepeat h-screen">
+	<HopsNav />
 
-<!--Page title-->
-<div class="mx-8 md:mx-20 mt-16 pb-4">
-	<h1 class="text-left text-6xl font-normal mb-4 dark:text-white">Tasks</h1>
-	<h2 class="text-2xl mb-4 text-grey dark:text-white dark:text-opacity-50">
-		Make things happen in one click
-	</h2>
-</div>
-
-<!--Tasks list-->
-<div class="mx-8 md:mx-20 pb-20 md:space-x-4">
-	<div class="grid md:grid-cols-4 gap-4">
-		<!--Task list items-->
-		{#each tasks as command}
-			<TaskItemCard {command} />
-		{/each}
+	<!--Page title-->
+	<div class="mx-8 md:mx-20 mt-16 pb-4">
+		<h1 class="text-left text-6xl font-normal mb-4 dark:text-white">Tasks</h1>
+		<h2 class="text-2xl mb-4 text-grey dark:text-white dark:text-opacity-50">
+			Make things happen in one click
+		</h2>
 	</div>
 
-	<!--Tasks empty state -->
-	{#if tasks.length === 0}
-		<div
-			class="bg-[url('/images/grain.jpg')] bg-cover flex items-center justify-center text-center rounded-lg mt-4 py-60 md:!ml-0"
-		>
-			<p class="text-white text-base font-medium">No tasks yet</p>
+	<!--Tasks list-->
+	<div class="mx-8 md:mx-20 pb-20 md:space-x-4">
+		<div class="grid md:grid-cols-4 gap-4">
+			<!--Task list items-->
+			{#each tasks as command}
+				<TaskItemCard {command} />
+			{/each}
 		</div>
-	{/if}
+
+		<!--Tasks empty state -->
+		{#if tasks.length === 0}
+			<div
+				class="bg-[url('/images/grain.jpg')] bg-cover flex items-center justify-center text-center rounded-lg mt-4 py-60 md:!ml-0"
+			>
+				<p class="text-white text-base font-medium">No tasks yet</p>
+			</div>
+		{/if}
+	</div>
 </div>
--- console/tailwind.config.js
+++ console/tailwind.config.js
@@ -25,7 +25,7 @@ const config = {
 			},
 			backgroundImage: {
 				grain : "url('/images/light-grain.png')",
-			}
+			},
 		}
 	}
 };
