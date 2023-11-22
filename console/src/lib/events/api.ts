ac654662e28a46b2920ae0b9277d20188502ec3c
Lukas Oberhuber <lukas@hiphops.io>
2023-11-22 19:35:13 +0000

    commented out code

--- console/src/lib/events/api.ts
+++ console/src/lib/events/api.ts
@@ -49,22 +49,6 @@ export function eventToTable(eventItem: EventItem): EventTable {
 	};
 }
 
-// export function ago(timestamp: string): string {
-// 	const now = new Date();
-// 	const then = new Date(timestamp);
-// 	const diff = now.getTime() - then.getTime();
-// 	const seconds = Math.floor(diff / 1000);
-// 	const minutes = Math.floor(seconds / 60);
-// 	const hours = Math.floor(minutes / 60);
-// 	const days = Math.floor(hours / 24);
-
-// 	if (days > 0) return `${days} days ago`;
-// 	if (hours > 0) return `${hours} hours ago`;
-// 	if (minutes > 0) return `${minutes} minutes ago`;
-// 	if (seconds > 0) return `${seconds} seconds ago`;
-// 	return 'just now';
-// }
-
 TimeAgo.addLocale(en);
 const timeAgo = new TimeAgo('en');
 
