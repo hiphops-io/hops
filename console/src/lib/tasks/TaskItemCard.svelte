2b9840b7a2e0cc19cf857c25899abdc65995a638
Zack <zack.strubel@gmail.com>
2023-11-22 11:55:30 +0000

    Added placeholder emoji (▶️) if user did not define emoji

--- console/src/lib/tasks/TaskItemCard.svelte
+++ console/src/lib/tasks/TaskItemCard.svelte
@@ -8,7 +8,7 @@
 	<div
 		class="px-6 py-8 bg-grey bg-opacity-5 rounded-xl dark:bg-white dark:bg-opacity-10 border border-white dark:border-black hover:border dark:hover:border hover:border-purple dark:hover:border-purple"
 	>
-		<h3 class="text-4xl mb-4">{command.emoji}</h3>
+		<h3 class="text-4xl mb-4">{command.emoji || '▶️'}</h3>
 		<p class="text-black font-medium dark:text-white">{command.display_name}</p>
 	</div>
 </a>
