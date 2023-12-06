e9687a49e99789401c1ad9b15d32b5fb2c829aef
Zack <62546213+zacksta@users.noreply.github.com>
2023-12-06 16:04:03 +0000

    Fix broken info icon (#24)
    Think images in components need {base}

--- console/src/lib/components/HelperText.svelte
+++ console/src/lib/components/HelperText.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	export let text: string;
 </script>
 
 <div class="text-lightgrey text-base flex items-start space-x-2">
-	<img src="/images/info-icon.svg" alt="info icon" />
+	<img src={`${base}/images/info-icon.svg`} alt="info icon" />
 	<p>
 		{@html text}
 	</p>
