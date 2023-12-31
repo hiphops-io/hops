37a345a78fef74e75b9aa0c7f125fb1745d05526
manterfield <tom@hiphops.io>
2023-12-31 15:50:38 +0000

    Cleaned up component loading/reloading logic

--- internal/hops/start.go
+++ internal/hops/start.go
@@ -24,11 +24,14 @@ type (
 	}
 
 	HopsServer struct {
-		HopsPath    string
-		KeyFilePath string
-		Logger      zerolog.Logger
-		ReplayEvent string
-		Watch       bool
+		HopsPath      string
+		KeyFilePath   string
+		Logger        zerolog.Logger
+		ReplayEvent   string
+		Watch         bool
+		reloadManager reload.Manager
+		runGroup      run.Group
+
 		HTTPServerConf
 		HTTPAppConf
 		K8sAppConf
@@ -41,8 +44,8 @@ type (
 
 	K8sAppConf struct {
 		KubeConfig  string
-		Serve       bool
 		PortForward bool
+		Serve       bool
 	}
 
 	RunnerConf struct {
@@ -55,10 +58,13 @@ func (h *HopsServer) Start(ctx context.Context) error {
 	defer rootCancel()
 
 	if !(h.RunnerConf.Serve || h.HTTPServerConf.Serve || h.K8sAppConf.Serve || h.HTTPAppConf.Serve) {
-		return errors.New("No serve functions are enabled. Nothing to do.")
+		return errors.New("No components are enabled. Nothing to do.")
 	}
 
-	// TODO: Should include this in runGroup too
+	if h.Watch {
+		h.reloadManager = reload.NewManager()
+	}
+
 	natsClient, err := h.startNATSClient()
 	if natsClient != nil {
 		defer natsClient.Close()
@@ -68,146 +74,147 @@ func (h *HopsServer) Start(ctx context.Context) error {
 		return err
 	}
 
-	var (
-		runGroup      run.Group
-		reloadManager = reload.NewManager()
-	)
-
 	hopsLoader, err := NewHopsFileLoader(h.HopsPath)
 	if err != nil {
-		h.Logger.Error().Err(err).Msg("Failed to read hops files")
-		return fmt.Errorf("Failed to read hops files: %w", err)
+		err := fmt.Errorf("Failed to read hops files: %w", err)
+		h.Logger.Error().Err(err).Msg("Start failed")
+		return err
 	}
 
-	reloadManager.Add(0, reload.ReloaderFunc(func(ctx context.Context, id string) error {
-		err := hopsLoader.Reload(ctx)
+	err = h.startHTTPServer(hopsLoader, natsClient)
+	if err != nil {
+		return err
+	}
+
+	err = h.startRunner(ctx, hopsLoader, natsClient)
+	if err != nil {
+		return err
+	}
+
+	err = h.startHTTPApp(ctx, natsClient)
+	if err != nil {
+		return err
+	}
+
+	err = h.startK8sApp(ctx, natsClient)
+	if err != nil {
+		return err
+	}
+
+	err = h.startReloader(ctx, hopsLoader)
+	if err != nil {
+		return err
+	}
+
+	return h.runGroup.Run()
+}
+
+func (h *HopsServer) startHTTPApp(ctx context.Context, natsClient *nats.Client) error {
+	if !h.HTTPAppConf.Serve {
+		return nil
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	start := func() error {
+		logger := h.Logger.With().Str("from", "httpapp").Logger()
+
+		httpApp, err := httpapp.NewHTTPHandler(ctx, natsClient, logger)
 		if err != nil {
-			h.Logger.Warn().Msgf("Hops files could not be reloaded: %s", err.Error())
+			return err
+		}
+
+		zlogger := logs.NewNatsZeroLogger(logger)
+		worker := worker.NewWorker(natsClient, httpApp, &zlogger)
+
+		// Blocks until complete or errored
+		return worker.Run(ctx)
+	}
+
+	h.runGroup.Add(
+		func() error {
+			return start()
+		},
+		func(_ error) {
+			cancel()
+		},
+	)
+
+	return nil
+}
+
+func (h *HopsServer) startHTTPServer(hopsLoader *HopsFileLoader, natsClient *nats.Client) error {
+	if !h.HTTPServerConf.Serve {
+		return nil
+	}
+
+	httpServer, err := NewHTTPServer(h.Address, hopsLoader, natsClient, h.Logger)
+	if err != nil {
+		return err
+	}
+
+	if h.Watch {
+		h.reloadManager.Add(10, reload.ReloaderFunc(func(ctx context.Context, id string) error {
+			return httpServer.Reload(ctx)
+		}))
+	}
+
+	h.runGroup.Add(
+		func() error {
+			return httpServer.Serve()
+		},
+		func(_ error) {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			err := httpServer.Shutdown(ctx)
+			if err != nil {
+				h.Logger.Error().Err(err).Msg("Unable to shut down http server")
+			}
+		},
+	)
+
+	return nil
+}
+
+func (h *HopsServer) startK8sApp(ctx context.Context, natsClient *nats.Client) error {
+	if !h.K8sAppConf.Serve {
+		return nil
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	start := func() error {
+		logger := h.Logger.With().Str("from", "k8sapp").Logger()
+
+		k8s, err := k8sapp.NewK8sHandler(ctx, natsClient, h.K8sAppConf.KubeConfig, h.K8sAppConf.PortForward, logger)
+		if err != nil {
+			return err
+		}
+
+		// Due to automated config loading, this worker may naturally decide to not work.
+		// This will interrupt all other components
+		if k8s == nil {
+			logger.Warn().Msg("Unable to load kubeconfig, exiting")
 			return nil
 		}
 
-		h.Logger.Info().Msg("Hops files reloaded")
-		return nil
-	}))
+		zlogger := logs.NewNatsZeroLogger(logger)
+		worker := worker.NewWorker(natsClient, k8s, &zlogger)
 
-	{
-		dirNotifier, err := NewDirNotifier(h.HopsPath)
-		if err != nil {
-			return err
-		}
-
-		// Add file watcher based reload notifier.
-		reloadManager.On(dirNotifier.Notifier())
-
-		ctx, cancel := context.WithCancel(ctx)
-		runGroup.Add(
-			func() error {
-				// Block forever until the watcher stops.
-				h.Logger.Info().Msgf("Watching %s for changes", h.HopsPath)
-				<-ctx.Done()
-				return nil
-			},
-			func(_ error) {
-				h.Logger.Info().Msg("Stopping hops file watcher")
-				dirNotifier.Close()
-				cancel()
-			},
-		)
+		// Blocks until complete or errored
+		return worker.Run(ctx)
 	}
 
-	if h.HTTPServerConf.Serve {
-		httpServer, err := NewHTTPServer(h.Address, hopsLoader, natsClient, h.Logger)
-		if err != nil {
-			return err
-		}
+	h.runGroup.Add(
+		func() error {
+			return start()
+		},
+		func(_ error) {
+			cancel()
+		},
+	)
 
-		reloadManager.Add(10, reload.ReloaderFunc(func(ctx context.Context, id string) error {
-			return httpServer.Reload(ctx)
-		}))
-
-		runGroup.Add(
-			func() error {
-				return httpServer.Serve()
-			},
-			func(_ error) {
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
-
-				err := httpServer.Shutdown(ctx)
-				if err != nil {
-					h.Logger.Error().Err(err).Msg("Unable to shut down http server")
-				}
-			},
-		)
-	}
-
-	if h.RunnerConf.Serve {
-		runner, err := NewRunner(natsClient, hopsLoader, h.Logger)
-		if err != nil {
-			return err
-		}
-
-		reloadManager.Add(10, reload.ReloaderFunc(func(ctx context.Context, id string) error {
-			return runner.Reload(ctx)
-		}))
-
-		ctx, cancel := context.WithCancel(ctx)
-		runGroup.Add(
-			func() error {
-				return runner.Run(ctx, nats.DefaultConsumerName)
-			},
-			func(_ error) {
-				cancel()
-			},
-		)
-	}
-
-	if h.HTTPAppConf.Serve {
-		ctx, cancel := context.WithCancel(ctx)
-		runGroup.Add(
-			func() error {
-				return startHTTPApp(ctx, natsClient, h.Logger)
-			},
-			func(_ error) {
-				cancel()
-			},
-		)
-	}
-
-	if h.K8sAppConf.Serve {
-		ctx, cancel := context.WithCancel(ctx)
-		runGroup.Add(
-			func() error {
-				return startK8sApp(
-					ctx,
-					natsClient,
-					h.K8sAppConf.KubeConfig,
-					h.K8sAppConf.PortForward,
-					h.Logger,
-				)
-			},
-			func(_ error) {
-				cancel()
-			},
-		)
-	}
-
-	{
-		ctx, cancel := context.WithCancel(ctx)
-		runGroup.Add(
-			func() error {
-				return reloadManager.Run(ctx)
-			},
-			func(_ error) {
-				// TODO: Check if error and log appropriately if so
-				// TODO: Avoid all reloader logic if watch not true
-				h.Logger.Info().Msg("Auto-reloading cancelled")
-				cancel()
-			},
-		)
-	}
-
-	return runGroup.Run()
+	return nil
 }
 
 func (h *HopsServer) startNATSClient() (*nats.Client, error) {
@@ -249,38 +256,89 @@ func (h *HopsServer) startNATSClient() (*nats.Client, error) {
 	return natsClient, nil
 }
 
-func startHTTPApp(ctx context.Context, natsClient *nats.Client, logger zerolog.Logger) error {
-	logger = logger.With().Str("from", "httpapp").Logger()
-
-	httpApp, err := httpapp.NewHTTPHandler(ctx, natsClient, logger)
-	if err != nil {
-		return err
-	}
-
-	zlogger := logs.NewNatsZeroLogger(logger)
-	worker := worker.NewWorker(natsClient, httpApp, &zlogger)
-
-	// Blocks until complete or errored
-	return worker.Run(ctx)
-}
-
-func startK8sApp(ctx context.Context, natsClient *nats.Client, kubeConfPath string, requiresPortForward bool, logger zerolog.Logger) error {
-	logger = logger.With().Str("from", "k8sapp").Logger()
-
-	k8s, err := k8sapp.NewK8sHandler(ctx, natsClient, kubeConfPath, requiresPortForward, logger)
-	if err != nil {
-		return err
-	}
-
-	// Due to automated config loading, this worker may naturally decide to not work.
-	// This will be logged by the worker. We just need to move on.
-	if k8s == nil {
+func (h *HopsServer) startReloader(ctx context.Context, hopsLoader *HopsFileLoader) error {
+	if !h.Watch {
 		return nil
 	}
 
-	zlogger := logs.NewNatsZeroLogger(logger)
-	worker := worker.NewWorker(natsClient, k8s, &zlogger)
+	h.reloadManager.Add(0, reload.ReloaderFunc(func(ctx context.Context, id string) error {
+		err := hopsLoader.Reload(ctx)
+		if err != nil {
+			h.Logger.Warn().Msgf("Hops files could not be reloaded: %s", err.Error())
+			return nil
+		}
 
-	// Blocks until complete or errored
-	return worker.Run(ctx)
+		h.Logger.Info().Msg("Hops files reloaded")
+		return nil
+	}))
+
+	{
+		dirNotifier, err := NewDirNotifier(h.HopsPath)
+		if err != nil {
+			return err
+		}
+
+		// Add file watcher based reload notifier.
+		h.reloadManager.On(dirNotifier.Notifier())
+
+		ctx, cancel := context.WithCancel(ctx)
+		h.runGroup.Add(
+			func() error {
+				// Block forever until the watcher stops.
+				h.Logger.Info().Msgf("Watching %s for changes", h.HopsPath)
+				<-ctx.Done()
+				return nil
+			},
+			func(_ error) {
+				h.Logger.Info().Msg("Stopping hops file watcher")
+				dirNotifier.Close()
+				cancel()
+			},
+		)
+	}
+
+	{
+		ctx, cancel := context.WithCancel(ctx)
+		h.runGroup.Add(
+			func() error {
+				return h.reloadManager.Run(ctx)
+			},
+			func(_ error) {
+				// TODO: Avoid all reloader logic if watch not true
+				h.Logger.Info().Msg("Auto-reloading cancelled")
+				cancel()
+			},
+		)
+	}
+
+	return nil
+}
+
+func (h *HopsServer) startRunner(ctx context.Context, hopsLoader *HopsFileLoader, natsClient *nats.Client) error {
+	if !h.RunnerConf.Serve {
+		return nil
+	}
+
+	runner, err := NewRunner(natsClient, hopsLoader, h.Logger)
+	if err != nil {
+		return err
+	}
+
+	if h.Watch {
+		h.reloadManager.Add(10, reload.ReloaderFunc(func(ctx context.Context, id string) error {
+			return runner.Reload(ctx)
+		}))
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	h.runGroup.Add(
+		func() error {
+			return runner.Run(ctx, nats.DefaultConsumerName)
+		},
+		func(_ error) {
+			cancel()
+		},
+	)
+
+	return nil
 }
